f234ac7a241ea40714da356c3b816b5298b26974
HerrZatacke <zatacke@gmail.com>
2020-12-25 21:06:46 +0100

    add virtual boundaries

--- src/javascript/app/store/middleware.js
+++ src/javascript/app/store/middleware.js
@@ -1,3 +1,24 @@
-const middleware = () => (next) => (action) => next(action);
+import calculateStats from '../../tools/calculateStats';
+
+const middleware = (store) => (next) => (action) => {
+
+  if (action.type === 'SET_POSITION') {
+    const state = store.getState();
+    const stats = calculateStats(action.position, state.params);
+
+    if (
+      stats.al > 87 ||
+      stats.ar > 87 ||
+      stats.al < 10 ||
+      stats.ar < 10 ||
+      action.position.y + 50 > state.params.height
+    ) {
+      return;
+    }
+
+  }
+
+  next(action);
+};
 
 export default middleware;
