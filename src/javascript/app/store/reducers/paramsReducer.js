c1adf9d2c5f49dd2578b2973d003ab9d813b785a
HerrZatacke <zatacke@gmail.com>
2020-12-26 16:50:22 +0100

    probably need 1:50 motor

--- src/javascript/app/store/reducers/paramsReducer.js
+++ src/javascript/app/store/reducers/paramsReducer.js
@@ -2,9 +2,8 @@ const paramsReducer = (value = {
   width: 1200, // mm
   height: 800, // mm
   gondolaWidth: 200, // mm
-  winchRadius: 20, // mm
+  winchRadius: 35, // mm
   weight: 2, // kg
-  // rpm: 250, // estimated / for 1:30 gear motor (no load speed 330rpm)
   rpm: 150, // estimated / for 1:50 gear motor (no load speed 200rpm)
 }, action) => {
   switch (action.type) {
