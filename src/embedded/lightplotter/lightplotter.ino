6a466340d8168af80255822b3fbe298c9f82fe51
HerrZatacke <zatacke@gmail.com>
2021-01-14 18:06:07 +0100

    esp - more control features

--- src/embedded/lightplotter/lightplotter.ino
+++ src/embedded/lightplotter/lightplotter.ino
@@ -17,32 +17,34 @@ const char ap_name[] = WIFI_CREDENTIALS_AP;
 
 WiFiServer server(80);
 
-String html1 = "<!DOCTYPE html><html><head><title>Control RoboClaw</title><meta charset='utf-8'><meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=yes'></head><body>";
-String html2 = "<button onClick=\"call('/motor1_on')\">motor 1 on</button> <code>roboclaw.ForwardM1(0x80, 63)</code><br>";
-String html3 = "<button onClick=\"call('/motor1_off')\">motor 1 off</button> <code>roboclaw.ForwardM1(0x80, 0)</code><br>";
-String html4 = "<button onClick=\"call('/motor2_on')\">motor 2 on</button> <code>roboclaw.ForwardM2(0x80, 63)</code><br>";
-String html5 = "<button onClick=\"call('/motor2_off')\">motor 2 off</button> <code>roboclaw.ForwardM2(0x80, 0)</code><br>";
+String html1 = "<!DOCTYPE html><html lang=\"en\"><head><title>Control RoboClaw</title><meta charset='utf-8'><meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=yes'></head><body>";
+String html2 = "<button onMouseDown=\"call('/motor1_reel_in')\" onMouseUp=\"call('/motors_stop')\">Reel in Motor 1</button><br>";
+String html3 = "<button onMouseDown=\"call('/motor1_unreel')\" onMouseUp=\"call('/motors_stop')\">Unreel Motor 1</button><br>";
+String html4 = "<button onMouseDown=\"call('/motor2_reel_in')\" onMouseUp=\"call('/motors_stop')\">Reel in Motor 2</button><br>";
+String html5 = "<button onMouseDown=\"call('/motor2_unreel')\" onMouseUp=\"call('/motors_stop')\">Unreel Motor 2</button><br>";
+String html6 = "<button onMouseDown=\"call('/motors_stop')\">Motors off</button><br>";
 #ifdef ADAFRUIT_NEOPIXEL_H
-String html6 = "<button onClick=\"call('/led_on')\">led on</button> <code>setColors(BRIGHT)</code><br>";
-String html7 = "<button onClick=\"call('/led_off')\">led off</button> <code>setColors(0)</code><br>";
+String html7 = "<button onMouseDown=\"call('/led_on')\">LED blink on</button> <code>setColors(BRIGHT)</code><br>";
+String html8 = "<button onMouseDown=\"call('/led_off')\">LED off</button> <code>setColors(0)</code><br>";
 #else
-String html6 = "<button onClick=\"call('/led_on')\">led on</button> <code>digitalWrite(LED, LOW)</code><br>";
-String html7 = "<button onClick=\"call('/led_off')\">led off</button> <code>digitalWrite(LED, HIGH)</code><br>";
+String html7 = "<button onMouseDown=\"call('/led_on')\">LED on</button> <code>digitalWrite(LED, LOW)</code><br>";
+String html8 = "<button onMouseDown=\"call('/led_off')\">LED off</button> <code>digitalWrite(LED, HIGH)</code><br>";
 #endif
-String html8 = "<button onClick=\"call('/test_1')\">test 2000 2000</button> <code>roboclaw.SpeedAccelDeccelPositionM1M2(0x80, ACCEL, SPEED, DECEL, 2000, ACCEL, SPEED, DECEL, 2000, 1)</code><br>";
-String html9 = "<button onClick=\"call('/test_2')\">test 2000 4000</button> <code>roboclaw.SpeedAccelDeccelPositionM1M2(0x80, ACCEL, SPEED, DECEL, 2000, ACCEL, SPEED, DECEL, 4000, 1)</code><br>";
-String html10 = "<button onClick=\"call('/test_3')\">test 4000 4000</button> <code>roboclaw.SpeedAccelDeccelPositionM1M2(0x80, ACCEL, SPEED, DECEL, 4000, ACCEL, SPEED, DECEL, 4000, 1)</code><br>";
-String html11 = "<button onClick=\"call('/test_4')\">test 4000 2000</button> <code>roboclaw.SpeedAccelDeccelPositionM1M2(0x80, ACCEL, SPEED, DECEL, 4000, ACCEL, SPEED, DECEL, 2000, 1)</code><br>";
-// String html12 = "<button onClick=\"fetch('/shape').then(updateInfo);\">shape</button><br>";
-// String html13 = "<button onClick=\"fetch('/flush').then(updateInfo);\">What does flush do?</button><br>";
-String html88 = "<pre id='status'></pre><script>const noConn = (err) => {document.querySelector('#status').innerText = err.toString()}; const updateInfo = (res) => res.json().then((info) => document.querySelector('#status').innerText = JSON.stringify(info, null, 2)); const call = (url) => {const controller = new AbortController(); const to = setTimeout(() => controller.abort(), 900); fetch(url, { signal: controller.signal }).then(updateInfo).then(() => {clearTimeout(to)}).catch(noConn);}; setInterval(() => {call('/info')}, 1000);</script>";
+String html9 = "<button onMouseDown=\"call('/test_1')\">test 2000 2000</button> <code>roboclaw.SpeedAccelDeccelPositionM1(0x80, ACCEL, SPEED, DECEL, 2000, 0); roboclaw.SpeedAccelDeccelPositionM2(0x80, ACCEL, SPEED, DECEL, 2000, 0);</code><br>";
+String html10 = "<button onMouseDown=\"call('/test_2')\">test 2000 4000</button> <code>roboclaw.SpeedAccelDeccelPositionM1(0x80, ACCEL, SPEED, DECEL, 2000, 0); roboclaw.SpeedAccelDeccelPositionM2(0x80, ACCEL, SPEED, DECEL, 4000, 0);</code><br>";
+String html11 = "<button onMouseDown=\"call('/test_3')\">test 4000 4000</button> <code>roboclaw.SpeedAccelDeccelPositionM1(0x80, ACCEL, SPEED, DECEL, 4000, 0); roboclaw.SpeedAccelDeccelPositionM2(0x80, ACCEL, SPEED, DECEL, 4000, 0);</code><br>";
+String html12 = "<button onMouseDown=\"call('/test_4')\">test 4000 2000</button> <code>roboclaw.SpeedAccelDeccelPositionM1(0x80, ACCEL, SPEED, DECEL, 4000, 0); roboclaw.SpeedAccelDeccelPositionM2(0x80, ACCEL, SPEED, DECEL, 2000, 0);</code><br>";
+// String html13 = "<button onMouseDown=\"fetch('/shape').then(updateInfo);\">shape</button><br>";
+// String html14 = "<button onMouseDown=\"fetch('/flush').then(updateInfo);\">What does flush do?</button><br>";
+String html15 = "<button onMouseDown=\"fetch('/reset_enc').then(updateInfo);\">Reset Encoders</button><br>";
+String html88 = "<pre id='status'>wait</pre><script>const stat = document.querySelector('#status'); const noConn = (err) => {stat.innerText = err.toString()}; const updateInfo = (res) => res.json().then((info) => { stat.style.background='#eee'; stat.innerText = JSON.stringify(info, null, 2); }); const call = (url) => {stat.style.background='#ccc'; const controller = new AbortController(); const to = setTimeout(() => controller.abort(), 900); fetch(url, { signal: controller.signal }).then(updateInfo).then(() => {clearTimeout(to)}).catch(noConn);}; setInterval(() => {call('/info')}, 1000);</script>";
 String html99 = "</body></html>";
 String request = "";
 
 #ifdef ADAFRUIT_NEOPIXEL_H
 #define PIN 5
 #define NUMPIXELS 1
-#define DELAYVAL 500
+#define DELAYVAL 100
 #define BRIGHT 63
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_RGB + NEO_KHZ800);
 unsigned long blink_nextMillis = 0;
@@ -157,31 +159,39 @@ void loop() {
     //if (request.indexOf("flush") > 0){ roboclaw.flush(); sendInfo(client); return; }
 
     else if (request.indexOf("test_1") > 0){
-      roboclaw.SpeedAccelDeccelPositionM1M2(0x80, ACCEL, SPEED, DECEL, 2000, ACCEL, SPEED, DECEL, 2000, 1);
+      roboclaw.SpeedAccelDeccelPositionM1(0x80, ACCEL, SPEED, DECEL, 2000, 0);
+      roboclaw.SpeedAccelDeccelPositionM2(0x80, ACCEL, SPEED, DECEL, 2000, 0);
       sendInfo(client); return;
     }
 
     else if (request.indexOf("test_2") > 0){
-      roboclaw.SpeedAccelDeccelPositionM1M2(0x80, ACCEL, SPEED, DECEL, 2000, ACCEL, SPEED, DECEL, 4000, 1);
+      roboclaw.SpeedAccelDeccelPositionM1(0x80, ACCEL, SPEED, DECEL, 2000, 0);
+      roboclaw.SpeedAccelDeccelPositionM2(0x80, ACCEL, SPEED, DECEL, 4000, 0);
       sendInfo(client); return;
     }
 
     else if (request.indexOf("test_3") > 0){
-      roboclaw.SpeedAccelDeccelPositionM1M2(0x80, ACCEL, SPEED, DECEL, 4000, ACCEL, SPEED, DECEL, 4000, 1);
+      roboclaw.SpeedAccelDeccelPositionM1(0x80, ACCEL, SPEED, DECEL, 4000, 0);
+      roboclaw.SpeedAccelDeccelPositionM2(0x80, ACCEL, SPEED, DECEL, 4000, 0);
       sendInfo(client); return;
     }
 
     else if (request.indexOf("test_4") > 0){
-      roboclaw.SpeedAccelDeccelPositionM1M2(0x80, ACCEL, SPEED, DECEL, 4000, ACCEL, SPEED, DECEL, 2000, 1);
+      roboclaw.SpeedAccelDeccelPositionM1(0x80, ACCEL, SPEED, DECEL, 4000, 0);
+      roboclaw.SpeedAccelDeccelPositionM2(0x80, ACCEL, SPEED, DECEL, 2000, 0);
       sendInfo(client); return;
     }
 
     // else if (request.indexOf("shape") > 0) { sendInfo(client); return; }
 
-    else if (request.indexOf("motor1_on") > 0){ roboclaw.ForwardM1(0x80, 63); sendInfo(client); return; }
-    else if (request.indexOf("motor1_off") > 0){ roboclaw.ForwardM1(0x80, 0); sendInfo(client); return; }
-    else if (request.indexOf("motor2_on") > 0){ roboclaw.ForwardM2(0x80, 63); sendInfo(client); return; }
-    else if (request.indexOf("motor2_off") > 0){ roboclaw.ForwardM2(0x80, 0); sendInfo(client); return; }
+#define REEL_SPEED 15
+
+    else if (request.indexOf("motor1_reel_in") > 0){ roboclaw.ForwardM1(0x80, REEL_SPEED); sendInfo(client); return; }
+    else if (request.indexOf("motor1_unreel") > 0){ roboclaw.BackwardM1(0x80, REEL_SPEED); sendInfo(client); return; }
+    else if (request.indexOf("motor2_reel_in") > 0){ roboclaw.ForwardM2(0x80, REEL_SPEED); sendInfo(client); return; }
+    else if (request.indexOf("motor2_unreel") > 0){ roboclaw.BackwardM2(0x80, REEL_SPEED); sendInfo(client); return; }
+    else if (request.indexOf("motors_stop") > 0){ roboclaw.ForwardM1(0x80, 0); roboclaw.ForwardM2(0x80, 0); sendInfo(client); return; }
+    else if (request.indexOf("reset_enc") > 0){ roboclaw.ResetEncoders(0x80); sendInfo(client); return; }
 
 #ifdef ADAFRUIT_NEOPIXEL_H
     else if (request.indexOf("led_on") > 0){ setColors(BRIGHT); sendInfo(client); return; }
@@ -191,21 +201,23 @@ void loop() {
     else if (request.indexOf("led_off") > 0){ digitalWrite(LED, HIGH); sendInfo(client); return;}
 #endif
 
-    client.print(html1);
-    client.print(html2);
-    client.print(html3);
-    client.print(html4);
-    client.print(html5);
-    client.print(html6);
-    client.print(html7);
-    client.print(html8);
-    client.print(html9);
-    client.print(html10);
-    client.print(html11);
-    // client.print(html12);
-    // client.print(html13);
+    client.println(html1);
+    client.println(html2);
+    client.println(html3);
+    client.println(html4);
+    client.println(html5);
+    client.println(html6);
+    client.println(html7);
+    client.println(html8);
+    client.println(html9);
+    client.println(html10);
+    client.println(html11);
+    client.println(html12);
+    // client.println(html13);
+    // client.println(html14);
+    client.println(html15);
 
-    client.print(html88);
-    client.print(html99);
+    client.println(html88);
+    client.println(html99);
   }
 }
